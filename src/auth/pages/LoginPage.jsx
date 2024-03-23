f93a79a08aeae2dcdb868b5f23a9f695758e8a11
David <david.gomez.martines@melia.com>
2024-03-23 11:53:12 +0100

    feat: update login with mui

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,15 +1,17 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
+
+import { Link as RouterLink } from 'react-router-dom';
 
 export const LoginPage = () => {
-	
-	const {login} = useContext(AuthContext)
-	
+	const { login } = useContext(AuthContext);
+
 	const navigate = useNavigate();
 
 	const onLogin = () => {
-
 		const lastPath = localStorage.getItem('last-path') || '/';
 
 		login('David Gomez');
@@ -19,12 +21,51 @@ export const LoginPage = () => {
 	};
 
 	return (
-		<div>
-			<h1>LoginPage</h1>
-			<hr />
-			<button className='btn btn-primary' onClick={onLogin}>
-				Login
-			</button>
-		</div>
+		<Grid
+			container
+			spacing={0}
+			direction='column'
+			alignItems='center'
+			justifyContent='center'
+			sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+		>
+			<Grid item className='box-shadow' xs={3} sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
+				<Typography variant='h5' sx={{ mb: 1 }}>
+					Login
+				</Typography>
+				<form>
+					<Grid container>
+						<Grid item xs={12} sx={{ mt: 2 }}>
+							<TextField label='Correo' type='email' placeholder='correo@google.com' fullWidth />
+						</Grid>
+						<Grid item xs={12} sx={{ mt: 2 }}>
+							<TextField label='Contraseña' type='password' placeholder='Contraseña' fullWidth />
+						</Grid>
+						<Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+							<Grid item xs={12} sm={6}>
+								<Button variant='contained' fullWidth>
+									Login
+								</Button>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<Button variant='contained' fullWidth>
+									<Google />
+									<Typography sx={{ ml: 1 }}>Google</Typography>
+								</Button>
+							</Grid>
+						</Grid>
+						<Grid container direction='row' justifyContent='end'>
+							<Link component={RouterLink} color='inherit' to='/auth/register'>
+								Crear una cuenta nueva
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+				<hr />
+				<button className='btn btn-primary' onClick={onLogin}>
+					Login
+				</button>
+			</Grid>
+		</Grid>
 	);
 };
