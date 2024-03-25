c58ea53114a90036100c25a1026170afa324c11a
David <david.gomez.martines@melia.com>
2024-03-25 11:47:55 +0100

    feat: add dotenv config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,17 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyDmLePcJE4qmu0Q9vuKvLMRssCQCIba6uY',
-	authDomain: 'vocabulary-app-57969.firebaseapp.com',
 	projectId: 'vocabulary-app-57969',
-	storageBucket: 'vocabulary-app-57969.appspot.com',
+	appId: process.env.FIREBASE_APP_ID,
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: '330528623149',
-	appId: '1:330528623149:web:70466853250ea4b10f46a4',
 };
 
 // Initialize Firebase
