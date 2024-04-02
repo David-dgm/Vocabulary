c2490ed9e432d16899d532b2e850af811692b237
David <david.gomez.martines@melia.com>
2024-04-02 08:03:11 +0200

    feat: update config env.test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
 	testEnvironment: 'jest-environment-jsdom',
-	// setupFiles: ['./jest.setup.js'],
+
+	setupFiles: ['./jest.setup.js'],
+	transformIgnorePatterns: ['node_modules/(?!(@firebase|sweetalert2|cloudinary)/)'],
 	moduleNameMapper: {
+		'^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/src/__mocks__/fileMock.js',
+		'^.+\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+
 		'^query-string$': '<rootDir>/node_modules/query-string-v7',
 	},
 };
